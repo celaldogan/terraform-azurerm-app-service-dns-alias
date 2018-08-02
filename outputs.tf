2257aad1d5ec8d2ab4ce97c56c2adfa0920b1812
Unknown <kevin@rustycog.com>
2018-08-02 16:24:32 +0100

    clean up

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
 output "fqdn" {
-  value = "${join(",",azurerm_app_service_custom_hostname_binding.default.*.hostname)}"
+  value = "${azurerm_app_service_custom_hostname_binding.default.*.hostname}"
 }
 
 output "app_service_names" {
-  value = "${join(",",azurerm_app_service_custom_hostname_binding.default.*.app_service_name)}"
+  value = "${azurerm_app_service_custom_hostname_binding.default.*.app_service_name}"
+}
+
+output "app_service_alias" {
+  value = "${zipmap(azurerm_app_service_custom_hostname_binding.default.*.app_service_name,azurerm_app_service_custom_hostname_binding.default.*.hostname)}"
 }
